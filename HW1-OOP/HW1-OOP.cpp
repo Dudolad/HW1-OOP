3414b2d63a342c7368ffbf830dd45a1aa8826682
dudolad_yaroslav <dudolad2009@gmail.com>
2025-07-14 16:14:00 +0300

    filled main

--- HW1-OOP/HW1-OOP.cpp
+++ HW1-OOP/HW1-OOP.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include "Student.h"
 using namespace std;
 
 int main()
 {
-    cout << "";
+    Student NN;
+    NN.showInfo();
+
+    Student Yaroslav("Dudolad Yaroslav Serhiyovich", 11.5, "Starosaltivskiy lyceum", 1);
+    Yaroslav.showInfo();
+
+    NN.setFullName("Petrov Ivan");
+    NN.setAverageMark(10.6);
+    NN.setSchoolName("Kherkiv School #1");
+    NN.setGroupNumber(26);
+    NN.showInfo();
+
+    cout << "Student NN's average mark: " << NN.getAverageMark() << endl;
+
+    return 0;
 }
